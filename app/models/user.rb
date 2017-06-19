768ec1a1117be487605237061b750f0e62f003b6
Moshe Naot <moshe.n@fiverr.com>
2017-06-19 16:38:39 +0300

    finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },   uniqueness: { case_sensitive: false }
  
   validates :password, presence: true, length: { minimum: 6 }
+  after_validation { self.errors.messages.delete(:password_digest) }
+
   validates :password_confirmation, presence: true
 
 end
